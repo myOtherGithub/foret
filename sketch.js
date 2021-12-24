436b899bfb6edf2c297fb4f0ca8359f8c2bdf45b
play.js <git@gituser.com>
2021-12-24 15:13:18 -0500

    added noise generation

---
+++ sketch.js
@@ -0,0 +1,43 @@
+/* eslint-disable no-undef, no-unused-vars */
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  // Put setup code here
+}
+
+function draw() {
+  // Put drawings here
+  fill(234, 31, 81);
+  noStroke();
+  rect(50, 50, 250, 250);
+  fill(255);
+  textStyle(BOLD);
+  textSize(140);
+  text("p5*", 60, 250);
+  for(let i=0; i<windowWidth; i++){
+    for(let j=0; j<windowHeight; j++){
+      let lightness = noise(i/10,j/11)*1255;
+      colorMode(HSB, 1255);
+      fill(lightness, 1255, 1255);
+      rect(i,j,1,1);
+    }
+  }
+  
+  
+  for(let i=0; i<windowWidth; i++){
+    for(let j=0; j<windowHeight; j++){
+      let lightness = noise(i/10,j/11)*1255;
+      colorMode(HSB, 1255);
+      fill(lightness, 1255, 1255);
+      rect(i,j,1,1);
+    }
+  }
+  
+  
+  noise()
+}
+
+// This Redraws the Canvas when resized
+windowResized = function() {
+  resizeCanvas(windowWidth, windowHeight);
+};
